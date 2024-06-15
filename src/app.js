121ddd22ce74ce81399406698a5ffaf57cf1c444
Gashu Salpagarov <gashusalpagarov@gmail.com>
2024-06-15 21:00:36 +0300

    fix db links

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
-require('dotenv').config();
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const connectDB = require('./config/db');
+const cronJobs = require('./utils/cronJobs');
+
 const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 const propertyRoutes = require('./routes/propertyRoutes');
 const syncRoutes = require('./routes/syncRoutes');
-const http = require('http');
-const socketIo = require('socket.io');
-const cronJobs = require('./utils/cronJobs');
+
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:8080',
+    credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
